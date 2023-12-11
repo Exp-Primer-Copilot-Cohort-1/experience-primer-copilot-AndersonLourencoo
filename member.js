21739b540d6607b377221a616d6f1619dd4957d4
Anderson Lourenço <44552949+AndersonLourencoo@users.noreply.github.com>
2023-12-11 10:03:54 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var skills = ["HTML", "CSS", "JS", "PHP"];
+    var member = {
+        name: "John",
+        age: 30,
+        skills: skills
+    };
+    console.log(member.skills[2]);
+}   
